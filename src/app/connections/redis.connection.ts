cf464316fecb2284921cdbe33fbbcf8dc8ecbeb6
salonibhingardive123 <saloni.bhingardive@coditas.com>
2026-06-23 15:21:23 +0530

    Update redis.connection.ts

--- src/app/connections/redis.connection.ts
+++ src/app/connections/redis.connection.ts
@@ -3,7 +3,7 @@ import { createClient } from "redis";
 export const redis = createClient({
   socket: {
     host: process.env.REDIS_HOST,
-    port: process.env.REDIS_PORT,
+    port: 6379,
     tls: true
   },
   password: process.env.REDIS_PASSWORD,
