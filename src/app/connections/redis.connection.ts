1779bd80a266902f19b16668a48b91081ace922a
salonibhingardive123 <saloni.bhingardive@coditas.com>
2026-06-23 06:01:10 +0530

    Update redis.connection.ts

--- src/app/connections/redis.connection.ts
+++ src/app/connections/redis.connection.ts
@@ -1,14 +1,23 @@
 import { createClient } from "redis";
 
-export const redis = createClient();
+export const redis = createClient({
+  socket: {
+    host: process.env.REDIS_HOST,
+    port: 6380,
+  },
+  password: process.env.REDIS_PASSWORD,
+});
+
+redis.on("error", (err) => {
+  console.error("Redis Error:", err);
+});
+
 export const connectToRedis = async () => {
   try {
     await redis.connect();
-    redis.on("error", (err) => {
-      console.log("error connecting to redis..");
-    });
     console.log("Connected to Redis successfully");
   } catch (error) {
+    console.error("Failed to connect to Redis:", error);
     throw error;
   }
 };
