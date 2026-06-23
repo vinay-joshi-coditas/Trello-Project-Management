10a9701a0677c04ce42500adc118fc3ff5794717
salonibhingardive123 <saloni.bhingardive@coditas.com>
2026-06-23 05:35:56 +0530

    Update config.cjs

--- config/config.cjs
+++ config/config.cjs
@@ -3,12 +3,16 @@ config();
 
 module.exports = {
   development: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
+    use_env_variable: "DATABASE_URL",
     dialect: "postgres",
-  },
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
+  }
+};
   // test: {
   //   username: "postgres",
   //   password: "coditas@8090",
